f10c17d2b71c0109608fa3fa7c61881e566db152
cbell108 <92697957+cbell108@users.noreply.github.com>
2024-04-22 23:07:52 -0400

    Add files via upload
    Updated files to fix looping errors, multiple edges between 2 nodes error cause by solver issue (u,v already reserved for residual edge)

--- GenerateGraph.py
+++ GenerateGraph.py
@@ -2,8 +2,8 @@
 # Number of vertices "v", number of source vertices "s", number of sinks "t".
 # Output:
 # Maximum flow of the graph, potentially the path(s) used.
-from math import isqrt
-from random import randint
+from random import randint, getrandbits
+
 
 class GenerateGraph:
     def __init__(self):
@@ -11,50 +11,67 @@ class GenerateGraph:
         self.V = 0
         self.max_C = 10
         self.source_sink_edges = 1
+        self.max_vertices = 4
 
     # Generate Graph
-    def newGraph(self, v, s, t):
+    def newGraph(self, v, s, t, allow_double_edges=False):
         self.V = v
+        edge_list = list()
         m_vertices = v - s - t
 
         if v > 2:
             # For each vertex
             for ve in range(m_vertices):
                 # Random number of edges
-                to_nodes = set([randint(0, m_vertices-1) for _ in range(randint(1, m_vertices))])
 
+                to_nodes = set([randint(0, m_vertices - 1) for _ in range(randint(1, self.max_vertices))])
+
+                # Check for edge to self
                 if ve in to_nodes:
                     # If multiple out-edges, remove
-                    if len(to_nodes) > 1:
-                        to_nodes.remove(ve)
-                    # Else add another
-                    else:
-                        if ve < m_vertices-1:
-                            to_nodes.add(ve+1)
+                    to_nodes.remove(ve)
+
+                    if len(to_nodes) < 1:
+                        if ve < m_vertices - 1:
+                            to_nodes.add(ve + 1)
                         else:
-                            to_nodes.add(ve-1)
-                [self.edges.append((ve, u, randint(1, self.max_C))) for u in to_nodes]
+                            to_nodes.add(ve - 1)
+                if not allow_double_edges:
+                    for u in to_nodes.copy():
+                        if (u, ve) in edge_list:
+                            if getrandbits(1):
+                                edge_list.remove((u, ve))
+                            else:
+                                to_nodes.remove(u)
+
+                [edge_list.append((ve, u)) for u in to_nodes]
 
         # Source
         for s in range(m_vertices, m_vertices + s):
-            to_nodes = set([randint(0, m_vertices - 1) for _ in range(randint(1, v//2+1))])
-            [self.edges.append((s, u, randint(1, self.max_C))) for u in to_nodes]
+            to_nodes = set([randint(0, m_vertices - 1) for _ in range(randint(1, self.max_vertices))])
+            [edge_list.append((s, u)) for u in to_nodes]
 
         # Sink
         for t in range(m_vertices + t, v):
-            from_nodes = set([randint(0, m_vertices - 1) for _ in range(randint(1, v // 2 + 1))])
-            [self.edges.append((v, t, randint(1, self.max_C))) for v in from_nodes]
+            from_nodes = set([randint(0, m_vertices - 1) for _ in range(randint(1, self.max_vertices))])
+            [edge_list.append((v, t)) for v in from_nodes]
 
         # Add in edges for skipped middle vertices:
-        in_edges = [u for _, u, _ in self.edges]
+        in_edges = [u for _, u in edge_list]
         no_in_edges = set(range(m_vertices)) - set(in_edges)
         for ve in no_in_edges:
-            u = randint(0, m_vertices-1)
+            u = randint(0, m_vertices - 1)
             if u == ve:
                 if u > 0:
                     u -= 1
                 else:
                     u += 1
-            self.edges.append((u, ve, randint(1, self.max_C)))
+            # if block can cause isolated/dead-end node errors
+            if not allow_double_edges:
+                if (ve, u) in edge_list:
+                    edge_list.remove((ve, u))
+            edge_list.append((u, ve))
 
-
+        # Add weights
+        for v, u in edge_list:
+            self.edges.append((v, u, randint(1, self.max_C)))
--- MaximumFlowMain.py
+++ MaximumFlowMain.py
@@ -13,25 +13,54 @@ import matplotlib.pyplot as plt
 num_vertices = 10
 num_source = 1
 num_sink = 1
+# Double edges, an edge from (u, v) and (v, u) breaks current graph implementation edmonds-karp
+allow_double_edges = False
 
 random_graph = GenerateGraph()
-random_graph.newGraph(num_vertices, num_source, num_sink)
+random_graph.newGraph(num_vertices, num_source, num_sink, allow_double_edges)
 
 graph_edges = random_graph.edges
-
-print(graph_edges)
-
-graph = nx.DiGraph()
-graph.add_weighted_edges_from(graph_edges)
-pos = nx.random_layout(graph)
-nx.draw(graph, with_labels=True, pos=pos, node_size=200, alpha=0.8, font_weight="bold", arrows=True)
-plt.show()
 
 source = 8
 sink = 9
 nodes = range(num_vertices)
 
-print(graph.edges)
+# Random Graph Alternative = nx.erdos_renyi_graph()
+graph = nx.DiGraph()
+graph.add_weighted_edges_from(graph_edges)
+# pos = nx.random_layout(graph)
+# pos = nx.spring_layout(graph)
+# Spiral seems to work the best for visuals:
+pos = nx.spiral_layout(graph)
+
+weights = nx.get_edge_attributes(graph, 'weight')
+
+bidirect_edges = [edge for edge in graph.edges if reversed(edge) in graph.edges]
+straight_edges = set(graph.edges) - set(bidirect_edges)
+
+bidirect_edge_labels = {}
+for v, u in bidirect_edges:
+    bidirect_edge_labels[(v, u)] = graph.get_edge_data(v, u)['weight']
+
+straight_edge_labels = {}
+for v, u in straight_edges:
+    straight_edge_labels[(v, u)] = graph.get_edge_data(v, u)['weight']
+
+
+nx.draw(graph, pos=pos, node_size=200, alpha=0.8)
+nx.draw_networkx_labels(graph, pos=pos, font_weight="bold")
+nx.draw_networkx_edge_labels(graph, pos=pos, edge_labels=bidirect_edge_labels, label_pos=.8, font_size=8)
+nx.draw_networkx_edge_labels(graph, pos=pos, edge_labels=straight_edge_labels, font_size=8)
+
+'''
+nx.draw_networkx_nodes(graph, pos=pos, node_size=200, alpha=0.8)
+nx.draw_networkx_labels(graph, pos=pos, font_weight="bold")
+nx.draw_networkx_edges(graph, pos=pos, edgelist=straight_edges)
+nx.draw_networkx_edges(graph, pos=pos, edgelist=bidirect_edges, connectionstyle='arc3,rad=.1')
+nx.draw_networkx_edge_labels(graph, pos=pos, edge_labels=nx.get_edge_attributes(graph, 'weight'), font_size=8)
+'''
 
 max_flow = EdmondsKarpSolver.ek_solve(int(source), int(sink), graph, nodes)
 print(f"Maximum flow is {max_flow}")
+
+plt.show()
